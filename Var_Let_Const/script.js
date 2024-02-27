ee04a96c093f4f899046a729eafe52a2999aa1ba
arraa <tiararahmah82@gmail.com>
2024-02-28 02:19:27 +0700

    var let const

---
+++ Var_Let_Const/script.js
@@ -0,0 +1,25 @@
+let halo = "hai";
+
+console.log(halo);
+
+// function tes() {
+//     for (var i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }
+
+// tes();
+// console.log(i);
+
+// (function(){
+//     for (var i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// })()
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+
